ca051853d5779a66ef2b9a5d763302d8fc1fc546
DESKTOP-LL832F4\SSAFY <wnddnjs83@ajou.ac.kr>
2024-03-13 13:48:20 +0900

    Create springboot project

---
+++ finfarm-backend/src/main/java/com/moneygang/finfarm/FinfarmApplication.java
@@ -0,0 +1,13 @@
+package com.moneygang.finfarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinfarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinfarmApplication.class, args);
+	}
+
+}
---
+++ finfarm-backend/src/test/java/com/moneygang/finfarm/FinfarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moneygang.finfarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinfarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
