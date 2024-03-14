11daad39ee37c4637845fb8d1c4e7e5e2221b0e3
이중원 <wnddnjs823@ajou.ac.kr>
2024-03-14 16:05:54 +0900

    Create new spring boot project (single module)

--- finfarm-backend/src/main/java/com/moneygang/finfarm/FinfarmApplication.java
+++ finfarm-backend/src/main/java/com/moneygang/finfarm/FinfarmApplication.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ExternalApplication {
+public class FinfarmApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ExternalApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(FinfarmApplication.class, args);
+	}
+
 }
