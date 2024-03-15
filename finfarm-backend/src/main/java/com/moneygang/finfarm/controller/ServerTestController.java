0ce187344942d20ed6aeb536418a2992c1190b58
오하빈 <dhdudgns80@naver.com>
2024-03-15 13:35:56 +0900

    devops : nginx check test method
    Nginx 동작 확인을 위한 테스트 메소드

---
+++ finfarm-backend/src/main/java/com/moneygang/finfarm/controller/ServerTestController.java
@@ -0,0 +1,16 @@
+package com.moneygang.finfarm.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin("*")
+public class ServerTestController {
+    @GetMapping
+    public String test(){
+        return "Good";
+    }
+}
