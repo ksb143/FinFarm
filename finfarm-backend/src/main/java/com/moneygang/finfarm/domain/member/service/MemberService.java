a4a1ca968bdad73afe4ee9ee8109b7e562809405
최재성 <jaisung0410@gmail.com>
2024-03-20 08:19:05 +0900

    feat : member login

--- finfarm-backend/src/main/java/com/moneygang/finfarm/domain/member/service/MemberService.java
+++ finfarm-backend/src/main/java/com/moneygang/finfarm/domain/member/service/MemberService.java
@@ -12,7 +12,6 @@ import com.moneygang.finfarm.global.base.JwtTokenProvider;
 import com.moneygang.finfarm.global.exception.GlobalException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
