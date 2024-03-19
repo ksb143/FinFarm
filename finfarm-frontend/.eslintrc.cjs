fe830dbc146f15ef090a4f6d3a9cab3b7cb3ae87
김수빈 <ksb143@naver.com>
2024-03-19 14:27:01 +0900

    :lipstick:Style: eslint, prettier 설정 적용

--- finfarm-frontend/.eslintrc.cjs
+++ finfarm-frontend/.eslintrc.cjs
@@ -3,15 +3,15 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',  // TypeScript 규칙을 적용합니다.
+    'plugin:react-hooks/recommended',  // React Hooks 규칙을 적용합니다.
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],  // 무시할 파일/폴더 패턴을 지정합니다.
+  parser: '@typescript-eslint/parser',  // TypeScript 파싱을 위한 파서를 지정합니다
+  plugins: ['react-refresh'],  // 사용할 플러그인 목록입니다.
   rules: {
     'react-refresh/only-export-components': [
-      'warn',
+      'warn',  // 컴포넌트만을 export할 때 경고를 출력합니다.
       { allowConstantExport: true },
     ],
   },
