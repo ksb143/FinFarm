ff722f734ecd50520af5626ec364666528c07458
김수빈 <ksb143@naver.com>
2024-03-20 11:02:15 +0900

    ✨ Feat: 로그인, 회원가입 로직 구현

--- fin-farm/.eslintrc.cjs
+++ fin-farm/.eslintrc.cjs
@@ -22,7 +22,11 @@ module.exports = {
 
   settings: {
     'import/resolver': {
-      typescript: {},
+      typescript: {
+        // Vite와 같이 사용하는 프로젝트의 tsconfig 경로를 지정합니다.
+        // project 키를 사용해 tsconfig.json의 위치를 정확히 지정할 수 있습니다.
+        project: './tsconfig.json',
+      },
     },
   },
 };
--- fin-farm/src/api/user.ts
+++ fin-farm/src/api/user.ts
@@ -1,3 +1,49 @@
 import localAxios from '../utils/http-commons';
+import { AxiosResponse, AxiosError } from 'axios';
 
-localAxios;
+const local = localAxios();
+
+interface LoginParams {
+  email: string; // 바디에 들어갈 이메일 주소
+}
+
+interface LoginResponse {
+  accessToken: string;
+  refreshToken: string;
+  nickname: string;
+  solveQuiz: boolean;
+  curPoint: number;
+  imageUrl: string;
+}
+
+interface JoinParams {
+  memberEmail: string; //회원 이메일
+  memberNickname: string; //회원 닉네임
+  memberAccountPassword: string; //계좌 비밀번호
+  memberImageUrl: string | null | void; //회원 사진
+}
+
+interface SuccessCallback<T> {
+  (response: AxiosResponse<T>): void;
+}
+interface FailCallback {
+  (error: AxiosError<{ message: string }>): void;
+}
+async function userLogin(
+  email: string,
+  success: SuccessCallback<LoginResponse>,
+  fail: FailCallback,
+): Promise<void> {
+  const params: LoginParams = { email };
+  await local.post(`/members/login`, params).then(success).catch(fail);
+}
+
+async function userJoin(
+  params: JoinParams,
+  success: SuccessCallback<{ message: string }>,
+  fail: FailCallback,
+): Promise<void> {
+  await local.post(`/members/sign-up`, params).then(success).catch(fail);
+}
+
+export { userLogin, userJoin };
