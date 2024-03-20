be55047e562597b0172c2350c46a060ede2ec0ee
최지원 <jwchoi.v1@gmail.com>
2024-03-20 20:50:57 +0900

    🎨 Installed all of needed package of libraries

---
+++ finfarm-frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-center">
+        <p>hello, world!</p>
+        <br />
+        <p>This is finfarm</p>
+        </h1>
+      
+    </>
+  )
+}
+
+export default App
