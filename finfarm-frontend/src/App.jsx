98457f8cd1110b19128399393652e347939b56f7
최지원 <jwchoi.v1@gmail.com>
2024-03-22 09:48:25 +0900

    👔 Login 화면 추가

--- finfarm-frontend/src/App.jsx
+++ finfarm-frontend/src/App.jsx
@@ -1,11 +1,23 @@
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from '@/components/layout/Navbar';
+import KakaoSocialLogin from '@/components/login/KakaoSocialLogin';
+import Home from '@/pages/home/Home';
 
 function App() {
   return (
-    <div className="min-h-screen px-32 bg-gray-50">
-      <Navbar></Navbar>
-    </div>
+    <Router>
+      <div className="min-h-screen px-32 bg-gray-50">
+        <Navbar></Navbar>
+        <Routes>
+          {/* 기본 경로를 KakaoSocialLogin으로 변경 */}
+          <Route path='/' element={<KakaoSocialLogin />} />
+          {/* 로그인 성공 후 볼 수 있는 Home 컴포넌트 경로 추가 */}
+          <Route path='/home' element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ finfarm-frontend/src/pages/home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useLocation } from 'react-router-dom'; // useLocation 훅 추가
+
+export default function Home() {
+    const location = useLocation(); // 현재 위치의 state에 접근
+    const name = location.state?.name; // 넘어온 사용자 이름을 받음
+
+    return(
+        <div>
+            <h1>this is home page</h1>
+            {/* 사용자 이름이 있다면 환영 메시지와 함께 표시 */}
+            <br />
+            {name && <p>환영합니다, {name}!</p>}
+        </div>
+    );
+}
