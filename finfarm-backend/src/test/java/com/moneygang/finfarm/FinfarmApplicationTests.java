527adc644fc9c3d6a16338efbe1679c1c326f901
최재성 <jaisung0410@gmail.com>
2024-03-22 09:30:00 +0900

    fix : FinfarmApplicationTests

--- finfarm-backend/src/test/java/com/moneygang/finfarm/FinfarmApplicationTests.java
+++ finfarm-backend/src/test/java/com/moneygang/finfarm/FinfarmApplicationTests.java
@@ -3,11 +3,11 @@ package com.moneygang.finfarm;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class FinfarmApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class FinfarmApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
