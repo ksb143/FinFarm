a013fabf87b691e134a967d3c0b333b43212b6cd
최재성 <jaisung0410@gmail.com>
2024-03-22 13:15:07 +0900

    fix : add CrossOrigin

--- finfarm-backend/src/main/java/com/moneygang/finfarm/FinfarmApplication.java
+++ finfarm-backend/src/main/java/com/moneygang/finfarm/FinfarmApplication.java
@@ -4,7 +4,9 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 @SpringBootApplication
 public class FinfarmApplication {
 
