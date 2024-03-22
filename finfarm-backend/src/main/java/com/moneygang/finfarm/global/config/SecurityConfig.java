343f72141bc489ac1edee60614f9d6baab0c24c4
최재성 <jaisung0410@gmail.com>
2024-03-22 09:10:54 +0900

    feat : apply Spring Security

---
+++ finfarm-backend/src/main/java/com/moneygang/finfarm/global/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.moneygang.finfarm.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf((csrf) -> csrf.disable())
+                .authorizeHttpRequests((authorizeRequests) ->
+                        authorizeRequests
+                                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll() // `/public/**` 경로는 인증 없이 접근 허용
+                                .requestMatchers("/member", "/member/login", "/member/sign-up").permitAll()
+                                .anyRequest().authenticated() // 나머지 모든 경로는 인증 필요
+                )
+                .build();
+    }
+
+}
