bccedd5e4a2498b170c8ded07ee4005af91a1cd4
Subin Kim <ksb143@naver.com>
2024-03-23 17:32:14 +0900

    💄 Style: 캐로셀 임시 저장

--- finfarm-frontend/src/components/bank/Ad.jsx
+++ finfarm-frontend/src/components/bank/Ad.jsx
@@ -1,27 +1,21 @@
-import adImage from '@/assets/images/adLoan.png';
-
-export default function Ad() {
+export default function Ad({ color, eventType, title, content, adImage }) {
   return (
-    <div className="flex rounded-lg justify-evenly items-center bg-amber-300 w-full">
+    <div
+      className={`flex w-full items-center justify-evenly rounded-lg ${color}`}
+    >
       <div>
         <div className="mb-10">
-          <i className="rounded-sm bg-white text-gray-500 border-solid border-2 border-gray-300 p-1 not-italic">
-            광고
+          <i className="rounded-sm border-2 border-solid border-gray-300 bg-white p-1 not-italic text-gray-500">
+            {eventType}
           </i>
         </div>
         <div>
-          <p className="font-hopang text-5xl mt-50 mb-3">
-            초보 농부들을 위한 <br />
-            퍼스트 파머론
-          </p>
-          <span>
-            초기 자금이 부족하고 신용이 없는 초보 농부들을 위한 <br />
-            상품 금리 10%의 낮은 이율로 누구나 쉽게 대출 가능
-          </span>
+          <p className="mt-50 mb-3 font-hopang text-5xl">{title}</p>
+          <span>{content}</span>
         </div>
       </div>
       <div>
-        <img src={adImage} alt="adImage" />
+        <img src={adImage} alt="광고 이미지" />
       </div>
     </div>
   );
--- finfarm-frontend/src/pages/bank/BankHome.jsx
+++ finfarm-frontend/src/pages/bank/BankHome.jsx
@@ -1,58 +1,84 @@
+import React, { useState } from 'react';
+
+import adImage1 from '@/assets/images/adLoan.png';
 import './BankHome.css';
 
 import Ad from '@/components/bank/Ad';
 import SquareButton from '@/components/layout/SquareButton';
 
 export default function BankHome() {
+  // 현재 보여져야 할 캐러셀 인덱스
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const adData = [
+    {
+      color: 'bg-amber-300',
+      title: '초보 농부들을 위한 퍼스트 파머론',
+      content:
+        '초기 자금이 부족하고 신용이 없는 초보 농부들을 위한 상품. 금리 10%의 낮은 이율로 누구나 쉽게 대출 가능',
+      adImage: adImage1,
+    },
+    // {
+    //   color: "bg-cyan-300",
+    //   title: ,
+    //   content: ,
+    //   adImage: adImage2
+    // },
+    // {
+    //   color: "bg-red-300",
+    //   title: ,
+    //   content: ,
+    //   adImage: adImage3
+    // },
+  ];
+  const items = [
+    <Ad
+      color="bg-amber-300"
+      eventType="광고"
+      title="초보 농부들을 위한 퍼스트 파머론"
+      content="초기 자금이 부족하고 신용이 없는 
+              초보 농부들을 위한 상품 금리 10%의 낮은 이율로 누구나 쉽게 대출 가능"
+      adImage={adImage}
+    />,
+    <Ad
+      color="bg-cyan-300"
+      eventType="광고"
+      title="초보 농부들을 위한 퍼스트 파머론"
+      content="초기 자금이 부족하고 신용이 없는 
+            초보 농부들을 위한 상품 금리 10%의 낮은 이율로 누구나 쉽게 대출 가능"
+      adImage={adImage}
+    />,
+    <Ad
+      color="bg-red-300"
+      eventType="광고"
+      title="초보 농부들을 위한 퍼스트 파머론"
+      content="초기 자금이 부족하고 신용이 없는 
+            초보 농부들을 위한 상품 금리 10%의 낮은 이율로 누구나 쉽게 대출 가능"
+      adImage={adImage}
+    />,
+  ];
+
+  // 이전으로 돌아가기
+  const goToPre = () => {
+    const isFirstItem = currentIndex === 0;
+    const newIndex = isFirstItem ? items.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  // 이 다음으로 돌아가기
+  const goToNext = () => {
+    const isLastItem = currentIndex === items.length - 1;
+    const newIndex = isLastItem ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
   return (
     <>
-      <div className="carousel w-full mb-5">
-        <div id="slide1" className="carousel-item relative w-full">
-          <Ad></Ad>
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide4" className="btn btn-circle">
-              ❮
-            </a>
-            <a href="#slide2" className="btn btn-circle">
-              ❯
-            </a>
-          </div>
-        </div>
-        <div id="slide2" className="carousel-item relative w-full">
-          <Ad></Ad>
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide1" className="btn btn-circle">
-              ❮
-            </a>
-            <a href="#slide3" className="btn btn-circle">
-              ❯
-            </a>
-          </div>
-        </div>
-        <div id="slide3" className="carousel-item relative w-full">
-          <Ad></Ad>
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide2" className="btn btn-circle">
-              ❮
-            </a>
-            <a href="#slide4" className="btn btn-circle">
-              ❯
-            </a>
-          </div>
-        </div>
-        <div id="slide4" className="carousel-item relative w-full">
-          <Ad className="w-full"></Ad>
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide3" className="btn btn-circle">
-              ❮
-            </a>
-            <a href="#slide1" className="btn btn-circle">
-              ❯
-            </a>
-          </div>
-        </div>
+      <div className="carousel-container">
+        <button onClick={goToPre}>❮</button>
+        <div className="carousel-item">{items[currentIndex]}</div>
+        <button onClick={goToNext}>❯</button>
       </div>
-      <div className="flex justify-center gap-5 bg-lime-100 py-5 mx-[-8rem]">
+      <div className="mx-[-8rem] flex justify-center gap-5 bg-lime-100 py-5">
         <SquareButton>조회</SquareButton>
         <SquareButton>입출금</SquareButton>
         <SquareButton>이체</SquareButton>
