c3aa2aa02bd80b53f59e31f8645f459f349c9393
최지원 <jwchoi.v1@gmail.com>
2024-03-25 08:32:57 +0900

    fix: 시작화면, 카카오로그인 진행

--- finfarm-frontend/src/App.jsx
+++ finfarm-frontend/src/App.jsx
@@ -2,9 +2,8 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from '@/components/layout/Navbar';
-import KakaoSocialLogin from '@/components/login/KakaoSocialLogin';
-import Home from '@/pages/home/Home';
-
+import Entrance from '@/pages/loginAndSignup/Entrance';
+import MainHome from '@/pages/home/MainHome';
 import BankHome from '@/pages/bank/BankHome';
 
 function App() {
@@ -13,10 +12,10 @@ function App() {
       <div className="min-h-screen px-32 bg-gray-50">
         <Navbar></Navbar>
         <Routes>
-          {/* 기본 경로를 KakaoSocialLogin으로 변경 */}
-          <Route path='/' element={<KakaoSocialLogin />} />
+          {/* 기본 경로를 Entrance으로 변경 */}
+          <Route path='/' element={<Entrance />} />
           {/* 로그인 성공 후 볼 수 있는 Home 컴포넌트 경로 추가 */}
-          <Route path='/home' element={<Home />} />
+          <Route path='/home' element={<MainHome />} />
           {/* 로그인 성공 후 볼 수 있는 bank 컴포넌트 경로 추가 */}
           <Route path="/bank" element={<BankHome />} />
         </Routes>
---
+++ finfarm-frontend/src/pages/loginAndSignup/Entrance.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import KakaoLoginButton from '@/components/loginAndSignup/KakaoLoginButton';
+
+export default function Entrance() {
+
+    return(
+        <div>
+            <h1>this is entrance page</h1>
+            <KakaoLoginButton />
+        </div>
+    );
+}
