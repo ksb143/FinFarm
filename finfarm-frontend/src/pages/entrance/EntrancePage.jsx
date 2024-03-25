66f40321828a0258ecaa1ecfc2ed9b810bc48b2c
최지원 <jwchoi.v1@gmail.com>
2024-03-25 22:22:35 +0900

    💄 입구 페이지 UI 변경 및 내농장 UI 설정중

--- finfarm-frontend/src/pages/entrance/EntrancePage.jsx
+++ finfarm-frontend/src/pages/entrance/EntrancePage.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import KakaoLoginButton from '@/components/entrance/KakaoLoginButton';
+import entrance1 from '@/assets/images/entrance1.png';
+import entrance2 from '@/assets/images/entrance2.png';
+import entrance3 from '@/assets/images/entrance3.png';
+import entrance4 from '@/assets/images/entrance4.png';
 
 export default function EntrancePage() {
 
     return(
         <div>
-            <h1>this is entrance page</h1>
-            <KakaoLoginButton />
+            <div className='flex justify-center'>
+                <p className='text-7xl'>this is entrance page</p>
+            </div>            
+            <br />
+            <br />
+            <div className='flex' >
+                <img src={entrance1} width='400px' />
+                <img src={entrance2} width='400px' />
+                <img src={entrance3} width='400px' />
+            </div>
+            <br />
+            <br />
+            <div className='flex justify-center' >
+                <KakaoLoginButton />
+            </div>
         </div>
     );
 }
--- finfarm-frontend/src/pages/myFarm/MyFarmPage.jsx
+++ finfarm-frontend/src/pages/myFarm/MyFarmPage.jsx
@@ -7,7 +7,7 @@ export default function MyFarmPage() {
             <h1>this is my farm page</h1>
             {/* 왼쪽: 5 * 5 밭이 있음. */}
             {/* 오른쪽: 창고(인벤토리 역할)이 존재함 */}
-            
+            {/* 상단:  */}
         </div>
     );
 }
