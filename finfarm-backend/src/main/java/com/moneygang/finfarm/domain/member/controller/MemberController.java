8393b97745b7fcfb2544b93e8ce06105244bef70
최재성 <jaisung0410@gmail.com>
2024-03-26 14:58:19 +0900

    feat : member quit

--- finfarm-backend/src/main/java/com/moneygang/finfarm/domain/member/controller/MemberController.java
+++ finfarm-backend/src/main/java/com/moneygang/finfarm/domain/member/controller/MemberController.java
@@ -3,10 +3,7 @@ package com.moneygang.finfarm.domain.member.controller;
 import com.moneygang.finfarm.domain.member.dto.request.MemberJoinRequest;
 import com.moneygang.finfarm.domain.member.dto.request.MemberLoginRequest;
 import com.moneygang.finfarm.domain.member.dto.request.MemberReissueRequest;
-import com.moneygang.finfarm.domain.member.dto.response.MemberAutoLoginResponse;
-import com.moneygang.finfarm.domain.member.dto.response.MemberJoinResponse;
-import com.moneygang.finfarm.domain.member.dto.response.MemberLoginResponse;
-import com.moneygang.finfarm.domain.member.dto.response.MemberReissueResponse;
+import com.moneygang.finfarm.domain.member.dto.response.*;
 import com.moneygang.finfarm.domain.member.entity.Member;
 import com.moneygang.finfarm.domain.member.service.MemberService;
 import jakarta.servlet.http.HttpServletRequest;
@@ -52,5 +49,10 @@ public class MemberController {
     @PostMapping("/reissue")
     public ResponseEntity<MemberReissueResponse> reissue(@RequestBody MemberReissueRequest request, @CookieValue(name = "refreshToken", defaultValue = "token") String refreshToken) {
         return memberService.reissue(request.getMemberEmail(), refreshToken);
+    }
+
+    @DeleteMapping("/quit")
+    public ResponseEntity<MemberQuitResponse> quit() {
+        return memberService.quit();
     }
 }
