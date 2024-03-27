6374c7b1bbae1259babf7d2c8150d3b20e5624fe
최지원 <jwchoi.v1@gmail.com>
2024-03-27 13:10:05 +0900

    🎨 카카오로그인 최최종

--- finfarm-frontend/src/pages/entrance/RedirectPage.jsx
+++ finfarm-frontend/src/pages/entrance/RedirectPage.jsx
@@ -3,7 +3,6 @@ import axios from 'axios';
 const { VITE_REACT_API_URL } = import.meta.env;
 
 const RedirectPage = () => {
-  
   useEffect(() => {
     // URL에서 인가 코드를 추출합니다.
     const auth_code0 = new URLSearchParams(window.location.search).get('code');
@@ -18,19 +17,26 @@ const RedirectPage = () => {
   }, []);
 
   const sendCodeToBackend = async (code) => {
-    const headers = {
-      "Content-Type" : "application/json",
-    }
-    try {
-      const response = await axios.post(`${VITE_REACT_API_URL}member/login`, 
-        JSON.stringify({ code }), // 데이터를 JSON 형태로 변환
-      { withCredentials: true, headers });
 
-    console.log('Success:', response.data);
+    const headers = {
+      "Content-Type": "application/json",
+    };
+  
+    const dataToSend = {
+      accessToken: code, // 여기서 'code'를 'accessToken' 키의 값으로 설정
+    };
+  
+    try {
+      const response = await axios.post(`${VITE_REACT_API_URL}member/login`,
+        JSON.stringify(dataToSend), // 수정된 데이터 객체를 JSON으로 변환
+      { withCredentials: true, headers });
+      console.log('Success:', response.data);
+      
     } catch (error) {
       console.error('Error:', error.response ? error.response.data : error.message);
     }
   };
+  
 
   return (
     <div>
