9f3abc7ffc7dac9c712d4d3706c7448889f4e520
최지원 <jwchoi.v1@gmail.com>
2024-03-27 14:03:11 +0900

    🎨 웹 로그인/회원가입 분기2

--- finfarm-frontend/src/components/entrance/KakaoLoginButton.jsx
+++ finfarm-frontend/src/components/entrance/KakaoLoginButton.jsx
@@ -4,8 +4,8 @@ import KakaoImg from '@/assets/images/kakao_login_large_wide.png'
 const KakaoLoginButton = ()=>{
   const buttonStyle = {
     background: `url(${KakaoImg}) center/cover`,
-    width: '600px', 
-    height: '70px', 
+    width: '300px', 
+    height: '40px', 
   };
   console.log('카카오로그인 버튼이 눌러졌음')
   return <button style={buttonStyle} onClick={startKakaoLogin}></button>;
--- finfarm-frontend/src/pages/entrance/EntrancePage.jsx
+++ finfarm-frontend/src/pages/entrance/EntrancePage.jsx
@@ -5,9 +5,9 @@ import entrance1 from '@/assets/images/entrance1.png';
 export default function EntrancePage() {
   return (
     <div>
-      <h1>this is entrance page</h1>
+      <h1 className='text-4xl'>this is entrance page</h1>
       <KakaoLoginButton />
-      <img src={entrance1}  width='300px' />;
+      <img src={entrance1}  width='300px' />
     </div>
   );
 }
