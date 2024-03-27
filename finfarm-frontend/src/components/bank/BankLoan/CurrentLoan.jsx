b3ca48ff67bf1d6c730f85dc77606de7a93545eb
김수빈 <ksb143@naver.com>
2024-03-27 22:38:02 +0900

    💄 Style: 뱅킹 대출페이지 UI 구현

--- finfarm-frontend/src/components/bank/BankLoan/CurrentLoan.jsx
+++ finfarm-frontend/src/components/bank/BankLoan/CurrentLoan.jsx
@@ -1,3 +1,74 @@
-export default function CurrentLoan() {
-  return <div className="roun border-2 border-gray-300 bg-white"></div>;
+import PropTypes from 'prop-types';
+
+CurrentLoan.propTypes = {
+  name: PropTypes.string,
+  interest: PropTypes.number,
+  startDate: PropTypes.instanceOf(Date),
+  endDate: PropTypes.instanceOf(Date),
+  amount: PropTypes.number,
+  repayAmount: PropTypes.number,
+  dDay: PropTypes.number,
+};
+
+export default function CurrentLoan({
+  name,
+  interest,
+  startDate,
+  endDate,
+  amount,
+  repayAmount,
+  dDay,
+}) {
+  const startLoanDate = startDate
+    .toLocaleString('ko-KR', {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+    })
+    .replace(/\.$/, '');
+  const endLoanDate = endDate
+    .toLocaleString('ko-KR', {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+    })
+    .replace(/\.$/, '');
+  return (
+    <div className="flex w-full flex-wrap items-center rounded-lg border-2 border-gray-300 bg-white p-5 lg:w-80">
+      <div className="flex w-3/4 flex-col gap-1">
+        <div className="flex items-center gap-10">
+          <h3 className="text-xl">{name}</h3>
+          <span className="text-sm">금리 주 {interest}%</span>
+        </div>
+        <p className="text-xs text-gray-500">
+          <span>{startLoanDate}</span>
+          <span>~</span>
+          <span>{endLoanDate}</span>
+        </p>
+        <div className="flex w-full">
+          <div className="w-1/2">대출 상환액</div>
+          <div className="w-1/2">{repayAmount.toLocaleString('ko-KR')}원</div>
+        </div>
+        <div className="flex w-full">
+          <div className="w-1/2">대출 금액</div>
+          <div className="w-1/2">{amount.toLocaleString('ko-KR')}원</div>
+        </div>
+        <div className="flex w-full">
+          <div className="w-1/2">최종 상환일까지</div>
+          <span
+            className={`w-1/2 ${dDay > 0 ? 'text-red-600' : 'text-blue-600'}`}
+          >
+            {dDay === 0
+              ? 'D-day'
+              : dDay > 0
+                ? `D-${dDay}`
+                : `D+${Math.abs(dDay)}`}
+          </span>
+        </div>
+      </div>
+      <button className="h-20 w-1/4  rounded-lg bg-lime-500 px-3 font-hopang text-xl text-white hover:border-4 hover:border-gray-300 hover:bg-lime-800">
+        상환하기
+      </button>
+    </div>
+  );
 }
--- finfarm-frontend/src/pages/bank/BankLoanHistoryPage.jsx
+++ finfarm-frontend/src/pages/bank/BankLoanHistoryPage.jsx
@@ -1,7 +1,131 @@
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+import CurrentLoan from '@/components/bank/BankLoan/CurrentLoan';
+import LoanHistory from '@/components/bank/BankLoan/LoanHistory';
+
 export default function BankLoanHistoryPage() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    pauseOnHover: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 1,
+        },
+      },
+    ],
+  };
+  const currentLoanData = [
+    {
+      pk: 0,
+      name: '퍼스트 파머론',
+      interest: 10,
+      period: 32,
+      amount: 30000000,
+      repayAmount: 33000000,
+      startDate: new Date('March 17, 2024 03:24:00'),
+      endDate: new Date('April 03 , 2024 03:24:00'),
+      dDay: 32,
+      isRepay: true,
+    },
+    {
+      pk: 0,
+      name: '퍼스트 파머론',
+      interest: 10,
+      period: 32,
+      amount: 300000,
+      repayAmount: 33000000,
+      startDate: new Date('March 17, 2024 03:24:00'),
+      endDate: new Date('April 03 , 2024 03:24:00'),
+      dDay: 32,
+      isRepay: true,
+    },
+    {
+      pk: 0,
+      name: '퍼스트 파머론',
+      interest: 10,
+      period: 32,
+      amount: 30000000,
+      repayAmount: 33000000,
+      startDate: new Date('March 17, 2024 03:24:00'),
+      endDate: new Date('April 03 , 2024 03:24:00'),
+      dDay: 32,
+      isRepay: true,
+    },
+    {
+      pk: 0,
+      name: '퍼스트 파머론',
+      interest: 10,
+      period: 32,
+      amount: 30000000,
+      repayAmount: 33000000,
+      startDate: new Date('March 17, 2024 03:24:00'),
+      endDate: new Date('April 03 , 2024 03:24:00'),
+      dDay: 32,
+      isRepay: true,
+    },
+    {
+      pk: 0,
+      name: '퍼스트 파머론',
+      interest: 10,
+      period: 32,
+      amount: 30000000,
+      repayAmount: 30000,
+      startDate: new Date('March 17, 2024 03:24:00'),
+      endDate: new Date('April 03 , 2024 03:24:00'),
+      dDay: 32,
+      isRepay: true,
+    },
+    {
+      pk: 0,
+      name: '퍼스트 파머론',
+      interest: 10,
+      period: 32,
+      amount: 30000000,
+      repayAmount: 330000,
+      startDate: new Date('March 17, 2024 03:24:00'),
+      endDate: new Date('April 03 , 2024 03:24:00'),
+      dDay: 32,
+      isRepay: true,
+    },
+  ];
+
+  const loanHistories = [
+    {
+      pk: 0,
+      name: '퍼스트 파머론',
+      interest: 10,
+      period: 32,
+      amount: 30000000,
+      repayAmount: 33000000,
+      startDate: new Date('March 17, 2024 03:24:00'),
+      endDate: new Date('April 03 , 2024 03:24:00'),
+      isRepay: true,
+    },
+  ];
+
   return (
     <div className="mx-24">
-      <div>
+      <div className="m">
         <h1 className="mb-5 text-2xl">대출 현황</h1>
         <div>
           <span className="mr-16">총 상환 금액</span>
@@ -11,7 +135,23 @@ export default function BankLoanHistoryPage() {
           <span className="mr-16">총 대출 금액</span>
           <span>총 {}</span>
         </div>
+        <Slider {...settings}>
+          {currentLoanData.map((currentDatum, idx) => (
+            <div className="mt-8 flex flex-wrap justify-center gap-5" key={idx}>
+              <CurrentLoan
+                name={currentDatum.name}
+                interest={currentDatum.interest}
+                startDate={currentDatum.startDate}
+                endDate={currentDatum.endDate}
+                amount={currentDatum.amount}
+                repayAmount={currentDatum.repayAmount}
+                dDay={currentDatum.dDay}
+              />
+            </div>
+          ))}
+        </Slider>
       </div>
+      <LoanHistory data={loanHistories} recordsView={15}></LoanHistory>
     </div>
   );
 }
