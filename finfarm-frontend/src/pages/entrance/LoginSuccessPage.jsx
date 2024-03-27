cc556136270afb14a0e77393b4f1f5a8d677c260
최지원 <jwchoi.v1@gmail.com>
2024-03-27 14:54:03 +0900

    🎨 회원가입 진행중

--- finfarm-frontend/src/pages/entrance/LoginSuccessPage.jsx
+++
@@ -1,8 +0,0 @@
-export default function LoginSuccessPage() {
-  return (
-    <div>
-      <h1>로그인에 성공했습니다.</h1>
-      <p>메인화면으로 진입하기 버튼</p>
-    </div>
-  );
-}
--- finfarm-frontend/src/router/Entrance.jsx
+++ finfarm-frontend/src/router/Entrance.jsx
@@ -1,15 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import EntrancePage from '@/pages/entrance/EntrancePage';
-import LoginPage from '@/pages/entrance/LoginPage';
-import SignupPage from '@/pages/entrance/SignupPage';
+import SignupProcessPage from '@/pages/entrance/SignupProcessPage';
 
 export default function Entrance() {
   return (
     <>
       <Routes>
         <Route path="/" element={<EntrancePage />} />
-        <Route path="login" element={<LoginPage />} />
-        <Route path="signup" element={<SignupPage />} />
+        <Route path="signup" element={<SignupProcessPage />} />
       </Routes>
     </>
   );
