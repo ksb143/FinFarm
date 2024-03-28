5209eccbfb48260520ff786b88b5f41a9952b481
최지원 <jwchoi.v1@gmail.com>
2024-03-28 20:17:34 +0900

    fix: 전역상태 관리  조정중.

--- finfarm-frontend/src/pages/entrance/RedirectPage.jsx
+++ finfarm-frontend/src/pages/entrance/RedirectPage.jsx
@@ -5,6 +5,14 @@ import axios from 'axios';
 const { VITE_REACT_API_URL } = import.meta.env;
 
 const RedirectPage = () => {
+  const { setPointsInthePocket, setProfileImageUrl, setNickname, setIsQuizSolved, setDateOfSignup, setEmail } = useUserStore(state => ({
+    setPointsInthePocket: state.setPointsInthePocket,
+    setProfileImageUrl: state.setProfileImageUrl,
+    setNickname: state.setNickname,
+    setIsQuizSolved: state.setIsQuizSolved,
+    setDateOfSignup: state.setDateOfSignup,
+    setEmail: state.setEmail,
+  }));
   
   const navigate = useNavigate();
   useEffect(() => {
@@ -42,11 +50,11 @@ const RedirectPage = () => {
       if (res.data.member) { // member: True 인 경우, 로그인 처리
         console.log(`안녕하세요, ${res.data.memberNickname}님! 환영합니다.`);
         // 받은 모든 정보를 zustand store에 저장하고, 메인홈으로 이동.
-        useUserStore.setPointsInthePocket(res.data.memberCurPoint);
-        useUserStore.setProfileImageUrlt(res.data.memberImageUrl);
-        useUserStore.setNickname(res.data.memberNickname); // 진짜 닉네임
-        useUserStore.setIsQuizSolved(res.data.memberSolveQuiz);
-        useUserStore.setDateOfSignup(res.data.memberCreateDate);
+        setPointsInthePocket(res.data.memberCurPoint);
+        setProfileImageUrl(res.data.memberImageUrl);
+        setNickname(res.data.memberNickname); // 진짜 닉네임을 저장함.
+        setIsQuizSolved(res.data.memberSolveQuiz);
+        setDateOfSignup(res.data.memberCreateDate);
 
         console.log('로그인완료. 메인화면으로 곧 이동합니다.')
         navigate('/home');
@@ -54,7 +62,7 @@ const RedirectPage = () => {
       } else { // member: False 인 경우, 회원가입 진행
         console.log('회원이 아닙니다. 회원가입 페이지로 이동하여 진행해주세요.');
         // 회원이 아닐 경우에도 백엔드로 이동하도록 수정할 수 있습니다.
-        useUserStore.setEmail(res.data.memberNickname);
+        setEmail(res.data.memberNickname); // 실제로 온 것은 이메일임.
         
         console.log('회원가입 준비 중. 이메일 저장완료. 회원가입 페이지로 곧 이동합니다.')
         navigate('/entrance/signup');
