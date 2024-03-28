c70a6fa96b39a6dfa6df6184bc27c9f8f7528952
김수빈 <ksb143@naver.com>
2024-03-28 23:34:18 +0900

    🐛 Fix: 컴포넌트 확인

--- finfarm-frontend/src/components/bank/BankTransfer/TransferDetail.jsx
+++ finfarm-frontend/src/components/bank/BankTransfer/TransferDetail.jsx
@@ -3,16 +3,16 @@ import profile_icon from '@/assets/images/profile_icon.png';
 
 TransferDetail.propTypes = {
   nickName: PropTypes.string,
-  transferDate: PropTypes.instanceOf(Date),
+  // transferDate: PropTypes.instanceOf(Date),
   profileImg: PropTypes.string,
 };
 
-export default function TransferDetail(nickName, transferDate, profileImg) {
+export default function TransferDetail(nickName, profileImg) {
   return (
     <div className="flex items-center justify-between rounded-lg border-2 border-solid border-gray-300 bg-white p-3">
       <div className="flex flex-col">
         <div>{nickName}</div>
-        <div className="text-xs text-gray-500">{transferDate}</div>
+        {/* <div className="text-xs text-gray-500">{transferDate}</div> */}
       </div>
       <div className="avatar">
         <div className="w-16 rounded-full">
