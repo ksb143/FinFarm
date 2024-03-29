47fa5659435533bd2060bc3a686d893c3df9f024
김수빈 <ksb143@naver.com>
2024-03-29 20:31:11 +0900

    ✨ Feat: 버튼 라우터 달기

--- finfarm-frontend/src/components/bank/BankBasicInfo.jsx
+++ finfarm-frontend/src/components/bank/BankBasicInfo.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { checkBalance } from '@/api/bank';
 import useUserStore from '@/store/userStore';
@@ -30,6 +31,12 @@ export default function BankBasicinfo({ isButton }) {
     fetchBalance(); // 비동기 함수 실행
   }, []);
 
+  // 네비게이션
+  const navigate = useNavigate();
+  const goToTransfer = () => {
+    navigate('/bank/transfer');
+  };
+
   return (
     <div className="flex w-full items-center justify-between rounded-xl border-2 border-solid border-gray-300 bg-white px-10 py-3">
       <div className="mr-3 flex-1 border-r-2 border-solid border-gray-300">
@@ -44,7 +51,7 @@ export default function BankBasicinfo({ isButton }) {
         </div>
         {isButton ? (
           <div className="flex gap-3">
-            <Button>계좌이체 하기</Button>
+            <Button onClickEvent={goToTransfer}>계좌이체 하기</Button>
             <Button>비밀번호 변경</Button>
           </div>
         ) : null}
