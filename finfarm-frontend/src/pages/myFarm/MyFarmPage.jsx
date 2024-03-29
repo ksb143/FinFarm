7df60805a0ac003db79eda2f15531211c2bbe475
최지원 <jwchoi.v1@gmail.com>
2024-03-29 15:43:29 +0900

    fix: 로딩 문제 바꾸기위해서 gif 줄임

--- finfarm-frontend/src/pages/myFarm/MyFarmPage.jsx
+++ finfarm-frontend/src/pages/myFarm/MyFarmPage.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import coming_soon from '@/assets/images/coming_soon.gif';
+import coming_soon from '@/assets/images/coming_soon.png';
 
 export default function MyFarmPage() {
 
     return(
         <div>
             <h1>MY farm page 나의 농장 준비중!</h1>
-            <img src={coming_soon} alt="img" width={800} />
+            <img src={coming_soon} alt="img" width={1000} />
         </div>
     );
 }
