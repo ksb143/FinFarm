b71c79cc377e311f37974e9ea8950bd4ccfe7a79
김수빈 <ksb143@naver.com>
2024-03-29 09:29:14 +0900

    💄 Style: gif 배경제거

--- finfarm-frontend/src/components/bank/BankBasicInfo.jsx
+++ finfarm-frontend/src/components/bank/BankBasicInfo.jsx
@@ -1,16 +1,20 @@
 import PropTypes from 'prop-types';
 
+import useUserStore from '@/store/userStore';
+
 BankBasicinfo.propTypes = {
   isButton: PropTypes.bool,
 };
 import Button from '@/components/layout/Button';
 
 export default function BankBasicinfo({ isButton }) {
-  const num = 23000;
+  const { nickname: nickname } = useUserStore((state) => ({
+    nickname: state.nickname,
+  }));
   return (
     <div className="flex w-full items-center justify-between rounded-xl border-2 border-solid border-gray-300 bg-white px-10 py-3">
       <div className="mr-3 flex-1 border-r-2 border-solid border-gray-300">
-        닉네임의 통장
+        `${nickname}님의 통장`
       </div>
       <div className="flex flex-1 items-center justify-between">
         <div className="flex gap-10 ps-3">
