2c3944fdb88536f8e679a0e1be4f2ed42996f657
이중원 <wnddnjs823@ajou.ac.kr>
2024-04-01 15:02:08 +0900

    feat: feat my page.js for define my page api

---
+++ finfarm-frontend/src/api/myPage.js
@@ -0,0 +1,27 @@
+import { localAxios } from '@/utils/http-common';
+
+const local = localAxios();
+
+// 마이페이지 프로필 사진 변경
+async function changeProfileImage(profileImage) {
+  try {
+    const response = await local.put('/member/profile', {});
+    console.log(response.data.isSuccess);
+    return response.data.isSuccess;
+  } catch (error) {
+    console.log('프로필 사진 저장', error);
+    throw error;
+  }
+}
+
+// 회원 탈퇴
+async function membershipWithdrawal() {
+  try {
+    const response = await local.put('/member/quit', {});
+    console.log(response.data.isSuccess);
+    return response.data.isSuccess;
+  } catch (error) {
+    console.log('회원탈퇴 실패', error);
+    throw error;
+  }
+}
