e9b86e0d6cc53892679b1dc43ca84a86904c9fb6
최지원 <jwchoi.v1@gmail.com>
2024-04-01 23:47:58 +0900

    feat: 창고 ui

---
+++ finfarm-frontend/src/components/myFarm/Inventory_test.jsx
@@ -0,0 +1,319 @@
+const Inventory_test = () => {
+  return (
+    <>
+      <div className="flex flex-col">
+        <div>
+          <div className="mb-3 bg-amber-200 text-center">
+            <h1 className="border-2 border-amber-700">창고</h1>
+          </div>
+        </div>
+        <div className="border-2 border-amber-400 bg-amber-400 px-3 py-3">
+          <div className="grid grid-cols-5 gap-5 bg-amber-700 px-2 py-2 text-center">
+            <div className="static h-40 w-40 bg-amber-100">
+              <div className="absolute size-6 rounded-full  bg-rose-300"></div>
+              <div className="mx-auto mb-1 mt-1 h-32 w-36 bg-amber-200"></div>
+              <div className="flex flex-row justify-between">
+                <div className="w-7 border-2 border-emerald-400 bg-white">
+                  +
+                </div>
+                <div className="flex-grow border-2 border-emerald-400 bg-white">
+                  0
+                </div>
+                <div className="w-7 border-2 border-emerald-400 bg-white">
+                  -
+                </div>
+              </div>
+            </div>
+            <div className="static h-40 w-40 bg-amber-100">
+              <div className="absolute size-6 rounded-full  bg-rose-300"></div>
+              <div className="mx-auto mb-1 mt-1 h-32 w-36 bg-amber-200"></div>
+              <div className="flex flex-row justify-between">
+                <div className="w-7 border-2 border-emerald-400 bg-white">
+                  +
+                </div>
+                <div className="flex-grow border-2 border-emerald-400 bg-white">
+                  0
+                </div>
+                <div className="w-7 border-2 border-emerald-400 bg-white">
+                  -
+                </div>
+              </div>
+            </div>
+            <div className="static h-40 w-40 bg-amber-100">
+              <div className="absolute size-6 rounded-full  bg-rose-300"></div>
+              <div className="mx-auto mb-1 mt-1 h-32 w-36 bg-amber-200"></div>
+              <div className="flex flex-row justify-between">
+                <div className="w-7 border-2 border-emerald-400 bg-white">
+                  +
+                </div>
+                <div className="flex-grow border-2 border-emerald-400 bg-white">
+                  0
+                </div>
+                <div className="w-7 border-2 border-emerald-400 bg-white">
+                  -
+                </div>
+              </div>
+            </div>
+            <div className="static h-40 w-40 bg-amber-100">
+              <div className="absolute size-6 rounded-full  bg-rose-300"></div>
+              <div className="mx-auto mb-1 mt-1 h-32 w-36 bg-amber-200"></div>
+              <div className="flex flex-row justify-between">
+                <div className="w-7 border-2 border-emerald-400 bg-white">
+                  +
+                </div>
+                <div className="flex-grow border-2 border-emerald-400 bg-white">
+                  0
+                </div>
+                <div className="w-7 border-2 border-emerald-400 bg-white">
+                  -
+                </div>
+              </div>
+            </div>
+            <div className="static h-40 w-40 bg-amber-100">
+              <div className="absolute size-6 rounded-full  bg-rose-300"></div>
+              <div className="mx-auto mb-1 mt-1 h-32 w-36 bg-amber-200"></div>
+              <div className="flex flex-row justify-between">
+                <div className="w-7 border-2 border-emerald-400 bg-white">
+                  +
+                </div>
+                <div className="flex-grow border-2 border-emerald-400 bg-white">
+                  0
+                </div>
+                <div className="w-7 border-2 border-emerald-400 bg-white">
+                  -
+                </div>
+              </div>
+            </div>
+            <div className="static h-40 w-40 bg-amber-100">
+              <div className="absolute size-6 rounded-full  bg-rose-300"></div>
+              <div className="mx-auto mb-1 mt-1 h-32 w-36 bg-amber-200"></div>
+              <div className="flex flex-row justify-between">
+                <div className="w-7 border-2 border-emerald-400 bg-white">
+                  +
+                </div>
+                <div className="flex-grow border-2 border-emerald-400 bg-white">
+                  0
+                </div>
+                <div className="w-7 border-2 border-emerald-400 bg-white">
+                  -
+                </div>
+              </div>
+            </div>
+            <div className="static h-40 w-40 bg-amber-100">
+              <div className="absolute size-6 rounded-full  bg-rose-300"></div>
+              <div className="mx-auto mb-1 mt-1 h-32 w-36 bg-amber-200"></div>
+              <div className="flex flex-row justify-between">
+                <div className="w-7 border-2 border-emerald-400 bg-white">
+                  +
+                </div>
+                <div className="flex-grow border-2 border-emerald-400 bg-white">
+                  0
+                </div>
+                <div className="w-7 border-2 border-emerald-400 bg-white">
+                  -
+                </div>
+              </div>
+            </div>
+            <div className="static h-40 w-40 bg-amber-100">
+              <div className="absolute size-6 rounded-full  bg-rose-300"></div>
+              <div className="mx-auto mb-1 mt-1 h-32 w-36 bg-amber-200"></div>
+              <div className="flex flex-row justify-between">
+                <div className="w-7 border-2 border-emerald-400 bg-white">
+                  +
+                </div>
+                <div className="flex-grow border-2 border-emerald-400 bg-white">
+                  0
+                </div>
+                <div className="w-7 border-2 border-emerald-400 bg-white">
+                  -
+                </div>
+              </div>
+            </div>
+            <div className="static h-40 w-40 bg-amber-100">
+              <div className="absolute size-6 rounded-full  bg-rose-300"></div>
+              <div className="mx-auto mb-1 mt-1 h-32 w-36 bg-amber-200"></div>
+              <div className="flex flex-row justify-between">
+                <div className="w-7 border-2 border-emerald-400 bg-white">
+                  +
+                </div>
+                <div className="flex-grow border-2 border-emerald-400 bg-white">
+                  0
+                </div>
+                <div className="w-7 border-2 border-emerald-400 bg-white">
+                  -
+                </div>
+              </div>
+            </div>
+            <div className="static h-40 w-40 bg-amber-100">
+              <div className="absolute size-6 rounded-full  bg-rose-300"></div>
+              <div className="mx-auto mb-1 mt-1 h-32 w-36 bg-amber-200"></div>
+              <div className="flex flex-row justify-between">
+                <div className="w-7 border-2 border-emerald-400 bg-white">
+                  +
+                </div>
+                <div className="flex-grow border-2 border-emerald-400 bg-white">
+                  0
+                </div>
+                <div className="w-7 border-2 border-emerald-400 bg-white">
+                  -
+                </div>
+              </div>
+            </div>
+            <div className="static h-40 w-40 bg-amber-100">
+              <div className="absolute size-6 rounded-full  bg-rose-300"></div>
+              <div className="mx-auto mb-1 mt-1 h-32 w-36 bg-amber-200"></div>
+              <div className="flex flex-row justify-between">
+                <div className="w-7 border-2 border-emerald-400 bg-white">
+                  +
+                </div>
+                <div className="flex-grow border-2 border-emerald-400 bg-white">
+                  0
+                </div>
+                <div className="w-7 border-2 border-emerald-400 bg-white">
+                  -
+                </div>
+              </div>
+            </div>
+            <div className="static h-40 w-40 bg-amber-100">
+              <div className="absolute size-6 rounded-full  bg-rose-300"></div>
+              <div className="mx-auto mb-1 mt-1 h-32 w-36 bg-amber-200"></div>
+              <div className="flex flex-row justify-between">
+                <div className="w-7 border-2 border-emerald-400 bg-white">
+                  +
+                </div>
+                <div className="flex-grow border-2 border-emerald-400 bg-white">
+                  0
+                </div>
+                <div className="w-7 border-2 border-emerald-400 bg-white">
+                  -
+                </div>
+              </div>
+            </div>
+            <div className="static h-40 w-40 bg-amber-100">
+              <div className="absolute size-6 rounded-full  bg-rose-300"></div>
+              <div className="mx-auto mb-1 mt-1 h-32 w-36 bg-amber-200"></div>
+              <div className="flex flex-row justify-between">
+                <div className="w-7 border-2 border-emerald-400 bg-white">
+                  +
+                </div>
+                <div className="flex-grow border-2 border-emerald-400 bg-white">
+                  0
+                </div>
+                <div className="w-7 border-2 border-emerald-400 bg-white">
+                  -
+                </div>
+              </div>
+            </div>
+            <div className="static h-40 w-40 bg-amber-100">
+              <div className="absolute size-6 rounded-full  bg-rose-300"></div>
+              <div className="mx-auto mb-1 mt-1 h-32 w-36 bg-amber-200"></div>
+              <div className="flex flex-row justify-between">
+                <div className="w-7 border-2 border-emerald-400 bg-white">
+                  +
+                </div>
+                <div className="flex-grow border-2 border-emerald-400 bg-white">
+                  0
+                </div>
+                <div className="w-7 border-2 border-emerald-400 bg-white">
+                  -
+                </div>
+              </div>
+            </div>
+            <div className="static h-40 w-40 bg-amber-100">
+              <div className="absolute size-6 rounded-full  bg-rose-300"></div>
+              <div className="mx-auto mb-1 mt-1 h-32 w-36 bg-amber-200"></div>
+              <div className="flex flex-row justify-between">
+                <div className="w-7 border-2 border-emerald-400 bg-white">
+                  +
+                </div>
+                <div className="flex-grow border-2 border-emerald-400 bg-white">
+                  0
+                </div>
+                <div className="w-7 border-2 border-emerald-400 bg-white">
+                  -
+                </div>
+              </div>
+            </div>
+            <div className="static h-40 w-40 bg-amber-100">
+              <div className="absolute size-6 rounded-full  bg-rose-300"></div>
+              <div className="mx-auto mb-1 mt-1 h-32 w-36 bg-amber-200"></div>
+              <div className="flex flex-row justify-between">
+                <div className="w-7 border-2 border-emerald-400 bg-white">
+                  +
+                </div>
+                <div className="flex-grow border-2 border-emerald-400 bg-white">
+                  0
+                </div>
+                <div className="w-7 border-2 border-emerald-400 bg-white">
+                  -
+                </div>
+              </div>
+            </div>
+            <div className="static h-40 w-40 bg-amber-100">
+              <div className="absolute size-6 rounded-full  bg-rose-300"></div>
+              <div className="mx-auto mb-1 mt-1 h-32 w-36 bg-amber-200"></div>
+              <div className="flex flex-row justify-between">
+                <div className="w-7 border-2 border-emerald-400 bg-white">
+                  +
+                </div>
+                <div className="flex-grow border-2 border-emerald-400 bg-white">
+                  0
+                </div>
+                <div className="w-7 border-2 border-emerald-400 bg-white">
+                  -
+                </div>
+              </div>
+            </div>
+            <div className="static h-40 w-40 bg-amber-100">
+              <div className="absolute size-6 rounded-full  bg-rose-300"></div>
+              <div className="mx-auto mb-1 mt-1 h-32 w-36 bg-amber-200"></div>
+              <div className="flex flex-row justify-between">
+                <div className="w-7 border-2 border-emerald-400 bg-white">
+                  +
+                </div>
+                <div className="flex-grow border-2 border-emerald-400 bg-white">
+                  0
+                </div>
+                <div className="w-7 border-2 border-emerald-400 bg-white">
+                  -
+                </div>
+              </div>
+            </div>
+            <div className="static h-40 w-40 bg-amber-100">
+              <div className="absolute size-6 rounded-full  bg-rose-300"></div>
+              <div className="mx-auto mb-1 mt-1 h-32 w-36 bg-amber-200"></div>
+              <div className="flex flex-row justify-between">
+                <div className="w-7 border-2 border-emerald-400 bg-white">
+                  +
+                </div>
+                <div className="flex-grow border-2 border-emerald-400 bg-white">
+                  0
+                </div>
+                <div className="w-7 border-2 border-emerald-400 bg-white">
+                  -
+                </div>
+              </div>
+            </div>
+            <div className="static h-40 w-40 bg-amber-100">
+              <div className="absolute size-6 rounded-full  bg-rose-300"></div>
+              <div className="mx-auto mb-1 mt-1 h-32 w-36 bg-amber-200"></div>
+              <div className="flex flex-row justify-between">
+                <div className="w-7 border-2 border-emerald-400 bg-white">
+                  +
+                </div>
+                <div className="flex-grow border-2 border-emerald-400 bg-white">
+                  0
+                </div>
+                <div className="w-7 border-2 border-emerald-400 bg-white">
+                  -
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Inventory_test;
--- finfarm-frontend/src/pages/myFarm/MyFarmPage.jsx
+++ finfarm-frontend/src/pages/myFarm/MyFarmPage.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 // import coming_soon from '@/assets/images/coming_soon.png';
-import Warehouse from '@/components/myFarm/Warehouse';
+import Inventory_test from '@/components/myFarm/Inventory_test';
 
 export default function MyFarmPage() {
   return (
     <div>
-      <h1 className="text-3xl">MY farm page 나의 농장 준비중!</h1>
-
-      <Warehouse />
+      <h1 className="text-center text-3xl">내 농장</h1>
+      <br />
+      <br />
+      <Inventory_test />
     </div>
   );
 }
