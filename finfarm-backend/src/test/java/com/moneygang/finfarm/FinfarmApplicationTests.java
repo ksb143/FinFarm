c1e4f84d0c0760413bcb8e82c3b6865e1410b1ac
오하빈 <dhdudgns80@naver.com>
2024-04-03 21:17:45 +0900

    devops : sonarQube code smell modify
    contextLoads() test code 비활성화

--- finfarm-backend/src/test/java/com/moneygang/finfarm/FinfarmApplicationTests.java
+++ finfarm-backend/src/test/java/com/moneygang/finfarm/FinfarmApplicationTests.java
@@ -1,10 +1,10 @@
 package com.moneygang.finfarm;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-@SpringBootTest
-class FinfarmApplicationTests {
-	@Test
-	void contextLoads() {
-	}
-}
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//@SpringBootTest
+//class FinfarmApplicationTests {
+//	@Test
+//	void contextLoads() {
+//	}
+//}
