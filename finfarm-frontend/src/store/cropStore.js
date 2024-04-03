40bbcab22b644866d61a9eabf480b8c15083a795
최지원 <jwchoi.v1@gmail.com>
2024-04-03 23:37:47 +0900

    feat: cropStore 관련 이미지 저장주소 변경

--- finfarm-frontend/src/store/cropStore.js
+++ finfarm-frontend/src/store/cropStore.js
@@ -2,28 +2,108 @@ import { create } from 'zustand';
 
 const useCropStore = create((set) => ({
   crop: [
-    { name: '수박', image: '/src/assets/images/watermelon.png' },
-    { name: '고구마', image: '/src/assets/images/sweetpotato.png' },
-    { name: '애호박', image: '/src/assets/images/squash.png' },
-    { name: '쌀', image: '/src/assets/images/rice.png' },
-    { name: '감자', image: '/src/assets/images/potato.png' },
+    {
+      name: '수박',
+      image:
+        'https://jaesbucket.s3.ap-northeast-2.amazonaws.com/+AgriculturalProducts/watermelon.png',
+    },
+    {
+      name: '고구마',
+      image:
+        'https://jaesbucket.s3.ap-northeast-2.amazonaws.com/+AgriculturalProducts/sweetpotato.png',
+    },
+    {
+      name: '애호박',
+      image:
+        'https://jaesbucket.s3.ap-northeast-2.amazonaws.com/+AgriculturalProducts/squash.png',
+    },
+    {
+      name: '쌀',
+      image:
+        'https://jaesbucket.s3.ap-northeast-2.amazonaws.com/20240403203441342',
+    },
+    {
+      name: '감자',
+      image:
+        'https://jaesbucket.s3.ap-northeast-2.amazonaws.com/+AgriculturalProducts/potato.png',
+    },
     { name: '고추가루', image: '/src/assets/images/pepperpowder.png' },
-    { name: '양파', image: '/src/assets/images/onion.png' },
-    { name: '대파', image: '/src/assets/images/greenonion.png' },
-    { name: '당근', image: '/public/images/carrot.png' },
-    { name: '배추', image: '/src/assets/images/cabbage.png' },
-    { name: '마늘', image: '@/assets/images/garlic.png' },
+    {
+      name: '양파',
+      image:
+        'https://jaesbucket.s3.ap-northeast-2.amazonaws.com/+AgriculturalProducts/onion.png',
+    },
+    {
+      name: '대파',
+      image:
+        'https://jaesbucket.s3.ap-northeast-2.amazonaws.com/+AgriculturalProducts/greenonion.png',
+    },
+    {
+      name: '당근',
+      image:
+        'https://jaesbucket.s3.ap-northeast-2.amazonaws.com/+AgriculturalProducts/carrot.png',
+    },
+    {
+      name: '배추',
+      image:
+        'https://jaesbucket.s3.ap-northeast-2.amazonaws.com/+AgriculturalProducts/cabbage.png',
+    },
+    {
+      name: '마늘',
+      image:
+        'https://jaesbucket.s3.ap-northeast-2.amazonaws.com/+AgriculturalProducts/galic.png',
+    },
     { name: '새싹', image: '/src/assets/images/sprout.png' },
-    { name: '고구마 씨앗', image: '/src/assets/images/seed.png' },
-    { name: '씨감자', image: '/src/assets/images/seed.png' },
-    { name: '마늘 씨앗', image: '/src/assets/images/seed.png' },
-    { name: '대파 씨앗', image: '/src/assets/images/seed.png' },
-    { name: '양파 씨앗', image: '/src/assets/images/seed.png' },
-    { name: '벼', image: '/src/assets/images/seed.png' },
-    { name: '당근 씨앗', image: '/src/assets/images/seed.png' },
-    { name: '배추 씨앗', image: '/src/assets/images/seed.png' },
-    { name: '수박 씨앗', image: '/src/assets/images/seed.png' },
-    { name: '애호박 씨앗', image: '/src/assets/images/seed.png' },
+    {
+      name: '고구마 씨앗',
+      image:
+        'https://jaesbucket.s3.ap-northeast-2.amazonaws.com/20240403203441342',
+    },
+    {
+      name: '씨감자',
+      image:
+        'https://jaesbucket.s3.ap-northeast-2.amazonaws.com/20240403203441342',
+    },
+    {
+      name: '마늘 씨앗',
+      image:
+        'https://jaesbucket.s3.ap-northeast-2.amazonaws.com/20240403203441342',
+    },
+    {
+      name: '대파 씨앗',
+      image:
+        'https://jaesbucket.s3.ap-northeast-2.amazonaws.com/20240403203441342',
+    },
+    {
+      name: '양파 씨앗',
+      image:
+        'https://jaesbucket.s3.ap-northeast-2.amazonaws.com/20240403203441342',
+    },
+    {
+      name: '벼',
+      image:
+        'https://jaesbucket.s3.ap-northeast-2.amazonaws.com/+AgriculturalProducts/rice.png',
+    },
+    {
+      name: '당근 씨앗',
+      image:
+        'https://jaesbucket.s3.ap-northeast-2.amazonaws.com/20240403203441342',
+    },
+    {
+      name: '배추 씨앗',
+      image:
+        'https://jaesbucket.s3.ap-northeast-2.amazonaws.com/20240403203441342',
+    },
+    {
+      name: '수박 씨앗',
+      image:
+        'https://jaesbucket.s3.ap-northeast-2.amazonaws.com/20240403203441342',
+    },
+    {
+      name: '애호박 씨앗',
+      image:
+        'https://jaesbucket.s3.ap-northeast-2.amazonaws.com/20240403203441342',
+    },
   ],
 }));
 
